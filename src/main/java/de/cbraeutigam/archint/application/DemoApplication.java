c637371164ee34a8c30cf10e280f9e38e17af9b5
Christof Bräutigam <rincewind@mathior.de>
2015-01-27 02:20:34 +0100

    Added cli to DemoApplication for standalone use, fixed path issues, added ant build file.

--- src/main/java/de/cbraeutigam/archint/application/DemoApplication.java
+++ src/main/java/de/cbraeutigam/archint/application/DemoApplication.java
@@ -19,6 +19,50 @@ import de.cbraeutigam.archint.util.ChecksumProvider;
 import de.cbraeutigam.archint.util.Ordering;
 
 public class DemoApplication {
+	
+	private enum Option {
+		HELP,
+		CREATE,
+		CHECK,
+		CONTAINS
+	}
+	
+	private class CliOptions {
+		
+		private Option option;
+		private Mode mode;
+		private String path1;
+		private String path2;
+		
+		public CliOptions(Option option, boolean full, String path1, String path2) {
+			this.option = option;
+			if (full) {
+				mode = Mode.FULL;
+			} else {
+				mode = Mode.ROOTS;
+			}
+			this.path1 = path1;
+			this.path2 = path2;
+			
+		}
+		
+		public Option getOption() {
+			return option;
+		}
+		
+		public Mode getMode() {
+			return mode;
+		}
+		
+		public String getPath1() {
+			return path1;
+		}
+		
+		public String getPath2() {
+			return path2;
+		}
+		
+	}
 
 	private final static String ORDERFILENAME = "ord.txt";
 	private final static String INTEGRITYFILENAME = "integrity.txt";
@@ -60,7 +104,7 @@ public class DemoApplication {
 		return hf1.contains(hf2);
 	}
 
-	public static void createIntegrityData(String baseDir, HashForest.Mode mode)
+	public static String createIntegrityData(String baseDir, HashForest.Mode mode)
 			throws NoSuchAlgorithmException, IOException {
 		File orderFile = new File(baseDir + File.separator + ORDERFILENAME);
 		if (orderFile.isFile()) {
@@ -77,8 +121,13 @@ public class DemoApplication {
 		Ordering ordering = new Ordering(new ChecksumProvider(
 				MessageDigest.getInstance("SHA-512")));
 		ordering.add(ORDERFILENAME);
+		/*
+		 * ordering should use relative paths, therefore use the full path and
+		 * cut the full path part of the base dir
+		 */
+		File baseDirF = new File(baseDir);
 		for (File f : fileTree) {
-			ordering.add(f.getAbsolutePath().substring(baseDir.length() + 1));
+			ordering.add(f.getAbsolutePath().substring(baseDirF.getAbsolutePath().length() + 1));
 		}
 		FileWriter fw = new FileWriter(orderFile);
 		ordering.writeTo(fw);
@@ -95,6 +144,24 @@ public class DemoApplication {
 		fw = new FileWriter(integrityFile);
 		hf.writeTo(fw);
 		fw.close();
+		
+		StringBuilder statusMessage = new StringBuilder();
+		statusMessage.append("Created integrity information for ");
+		statusMessage.append(baseDir);
+		statusMessage.append("\n");
+		statusMessage.append("Files: ");
+		statusMessage.append(ordering.getIdentifiers().size());
+		statusMessage.append("\n");
+		statusMessage.append("Mode: ");
+		statusMessage.append(mode.toString());
+		statusMessage.append("\n");
+		statusMessage.append("Integrity information written to: ");
+		statusMessage.append(integrityFile.getAbsolutePath());
+		statusMessage.append("\n");
+		statusMessage.append("Ordering information written to: ");
+		statusMessage.append(orderFile.getAbsolutePath());
+		statusMessage.append("\n");
+		return statusMessage.toString();
 	}
 
 	private static List<File> listFileTree(File dir) {
@@ -165,21 +232,142 @@ public class DemoApplication {
 		fr.close();
 		return ordering;
 	}
+	
+	
+	private static void printUsageAndExit(int exitStatus, String errorMessage) {
+		String usageMessage = new String("Usage: java -jar demo.jar [OPTIONS] PATHS\n"
+				+ "Create integrity information or test integrity of all files in PATH.\n"
+				+ "Except for --full only the first option is considered, others are ignored.\n"
+				+ "OPTIONS\n"
+				+ "  -h  Help (output this text)\n"
+				+ "  -c  Create integrity information for PATH.\n"
+				+ "  -t  Test integrity information for PATH.\n"
+				+ "  -p  Test if the first path is an extended version of the second path.\n"
+				+ "      (experimental)\n"
+				+ "  --full  Create full integrity information instead of just the roots.\n"
+				+ "          Works only with -c, is ignored otherwise.\n");
+		if (errorMessage != null && !errorMessage.equals("")) {
+			usageMessage = "Error: " + errorMessage + "\n" + usageMessage;
+		}
+		if (exitStatus == 0) {
+			System.out.println(usageMessage);
+		} else {
+			System.err.println(usageMessage);
+		}
+		
+		System.exit(exitStatus);
+	}
+	
+	
+	private static void checkPath(String path) {
+		if (path == null) {
+			printUsageAndExit(1, "Path argument missing."); 
+		}
+		
+		File pathF = new File(path);
+		
+		if (!pathF.isDirectory()) {
+			printUsageAndExit(1, "Path is not a directory: " + path); 
+		}
+		if (!pathF.canRead()) {
+			printUsageAndExit(1, "Path is not readable: " + path); 
+		}
+		
+	}
+	
+	
+	private CliOptions parseArgs(String[] args) {
+		
+		Option option = null;
+		boolean isFull = false;
+		
+		for (int i = args.length - 1; i >= 0; --i) {
+			String arg = args[i];
+			if (arg.equals("--full")) {
+				isFull = true;
+			} else if (arg.equals("-h")) {
+				option = Option.HELP;
+			} else if (arg.equals("-c")) {
+				option = Option.CREATE;
+			} else if (arg.equals("-t")) {
+				option = Option.CHECK;
+			} else if (arg.equals("-p")) {
+				option = Option.CONTAINS;
+			}
+		}
+		
+		if (option == null) {
+			printUsageAndExit(1, "No option given.");
+		}
+		
+		if (option.equals(Option.HELP)) {
+			return new CliOptions(option, isFull, null, null);
+		}
+		
+		if (option.equals(Option.CONTAINS)) {
+			if (args.length < 3) {
+				printUsageAndExit(1, "Two paths expected.");
+			}
+			String path1 = args[args.length - 2];
+			String path2 = args[args.length - 1];
+			checkPath(path1);
+			checkPath(path2);
+			return new CliOptions(option, isFull, path1, path2);
+		}
+		
+		String path = args[args.length - 1];
+		checkPath(path);
+		return new CliOptions(option, isFull, path, null);
+	}
+	
 
 	public static void main(String[] args) throws NoSuchAlgorithmException,
 			IOException {
-		String homeDir = System.getProperty("user.home");
-		String testDirCurrent = homeDir
-				+ "/Desktop/projekt-digitales-magazin/testcases";
-		String testDirOld = homeDir
-				+ "/Desktop/projekt-digitales-magazin/testcases-old";
-		createIntegrityData(testDirCurrent, Mode.ROOTS);
-		createIntegrityData(testDirOld, Mode.ROOTS);
-
-		System.out.println(checkIntegrity(testDirCurrent));
-		System.out.println(checkIntegrity(testDirOld));
 		
-		System.out.println(checkContains(testDirCurrent, testDirOld));
+//		String homeDir = System.getProperty("user.home");
+//		String testDirCurrent = homeDir
+//				+ "/Desktop/projekt-digitales-magazin/testcases";
+//		String testDirOld = homeDir
+//				+ "/Desktop/projekt-digitales-magazin/testcases-old";
+//		
+//		args = new String[] {"-c", "--full", testDirCurrent};
+		
+		DemoApplication da = new DemoApplication();
+		CliOptions opts = da.parseArgs(args);
+		
+		if (opts.getOption().equals(Option.HELP)) {
+			printUsageAndExit(0, null);
+		} else if (opts.getOption().equals(Option.CREATE)) {
+			System.out.println(createIntegrityData(opts.getPath1(), opts.getMode()));
+		} else if (opts.getOption().equals(Option.CHECK)) {
+			if (checkIntegrity(opts.getPath1())) {
+				System.out.println("VALID");
+			} else {
+				System.out.println("INVALID");
+			}
+		} else if (opts.getOption().equals(Option.CONTAINS)) {
+			if (checkContains(opts.getPath1(), opts.getPath2())) {
+				System.out.println("VALID");
+				System.out.println(opts.getPath1());
+				System.out.println("is an extended version of");
+				System.out.println(opts.getPath2());
+			} else {
+				System.out.println("INVALID");
+				System.out.println(opts.getPath1());
+				System.out.println("is NOT an extended version of");
+				System.out.println(opts.getPath2());
+			}
+		} 
+		
+		
+		
+//		createIntegrityData(testDirCurrent, Mode.ROOTS);
+//		createIntegrityData(testDirOld, Mode.ROOTS);
+//
+//		System.out.println(checkIntegrity(testDirCurrent));
+//		System.out.println(checkIntegrity(testDirOld));
+//		
+//		System.out.println(checkContains(testDirCurrent, testDirOld));
 		
 	}
 
